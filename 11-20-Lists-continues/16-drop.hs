92476aad9710310e6be13a59c20a6f5f063d99eb
Dima Zudochkin <dnd.pliz@gmail.com>
2015-02-11 22:55:21 +0300

    16-drop every N'th element from list solved :shit:

---
+++ 11-20-Lists-continues/16-drop.hs
@@ -0,0 +1,15 @@
+main :: IO()
+
+remove' :: [a] -> Int -> Int -> [a] -> [a]
+remove' [] _ _ _ = []
+remove' (x:xs) index i a =
+-- TODO: refactor this :shit:
+  if (index + 1) `mod` i == 0
+  then [] ++ remove' xs (succ index) i a;
+  else [a !! index] ++ remove' xs (succ index) i a;
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery a i = remove' a 0 3 a
+
+
+main = print $ dropEvery "abcdefghik" 3
